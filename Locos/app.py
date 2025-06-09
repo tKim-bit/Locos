c125e97d49e7f1188bf50f189fe5b3966c70363c
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-06-09 14:25:23 +0900

    Update app.py

--- Locos/app.py
+++ Locos/app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 from streamlit_javascript import st_javascript
-import Data.Review
-import Data.ShopData
-import Data.UserData
+from Service import Service
 
 def main():
     st.title('locos\n-地域のお店発見-')
