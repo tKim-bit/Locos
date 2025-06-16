88897f35d39c891401248c8a7e24d82b3676eeef
kuronoa967 <st324a2111r.taka@urayama.ac.jp>
2025-06-16 14:20:46 +0900

    Update app.py

--- Locos/app.py
+++ Locos/app.py
@@ -3,47 +3,53 @@ import streamlit.components.v1 as components
 import pandas as pd
 from streamlit_javascript import st_javascript
 
+from Service import Service
+
 
 def main():
     st.title('locos\n-地域のお店発見-')
     screen = st.sidebar.selectbox("", ["ホーム", "クーポン", "ブックマーク"])
     if screen == "ホーム":
+        get_geolocation()
         st.button('周辺の店舗検索')
-        st.map()
+        st.map(st.session_state.geoloc)
     elif screen == "クーポン":
+        st.title('ブックマーク一覧')
         st.text_input("店名検索")
+        st.dataframe(df, column_config={"shopData":st.column_config.LinkColumn("店名", )})
     elif screen == "ブックマーク":
-        df = pd.DataFrame({"shopData":["http://sample.locos_app.py"],})
+        st.title('ブックマーク一覧')
+        df = pd.read_csv()
         st.dataframe(df, column_config={"shopData":st.column_config.LinkColumn("店名", display_text="http://(.*?)\.locos_app.py")},)
 
 def get_geolocation():
-    components.html(
+    # JSで現在地取得
+    coords = st_javascript(
         """
-        <script>
         navigator.geolocation.getCurrentPosition(
-            function(position){
-                console.log("位置情報が取得できました。", );
-            },
-            function(error) {
-                condole.log("位置情報の取得に失敗しました。", error.message)
+            (position) => {
+                const coords = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+                window.parent.postMessage({type: 'streamlit:setComponentValue', value: coords}, '*')
             }
-        )
-        </script>
-        """,
-        height=0
-    )
-    
-    result = st_javascript(
-        """
-        new Promise((resolve) => {
-            window.addEventListener("message", (event) => {
-                resolve(event.data);
-            }, {once: true});
-        });
+        );
         """
     )
     
-    return result
-
+    st.session_state['geoloc'] = coords
+    st.text(type(coords))
+    
+    # 現在地が取得できたら店情報を表示
+    if coords and "latitude" in coords and "longitude" in coords:
+        service = Service()
+        fullShopList = service.getShopList()  # 既存の詳細付きデータ
+        result = service.get_nearby_places(coords["latitude"], coords["longitude"], shopList=fullShopList)
+        st.write("付近の店:")
+        for shop in result:
+            st.markdown(f"- **{shop.name}**（{shop.adress}）")
+    else:
+        st.write("位置情報の取得を許可してください。")
 
 main()
