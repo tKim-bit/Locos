c0828eb39ae4ddab99933c7fe6e21463cce43ea5
tKim-bit <st324a2114k.tanii@urayama.ac.jp>
2025-07-07 13:33:48 +0900

    Update Service.py

--- Locos/Service/Service.py
+++ Locos/Service/Service.py
@@ -14,7 +14,7 @@ class Service:
             next(reader)
             for row in reader:
                 shop=ShopData(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],
-                                row[8],row[9],row[10],row[11],row[12])
+                                row[8],row[9],row[10],row[11],row[12],row[13],row[14])
                 shopList.append(shop)
         return shopList
     
